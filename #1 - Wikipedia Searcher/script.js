d1085ef69578141705a50f450584960e02135af9
Debidatta Mohanta <debidattamohanta1@gmail.com>
2024-06-22 16:25:04 +0530

    done

--- #1 - Wikipedia Searcher/script.js
+++ #1 - Wikipedia Searcher/script.js
@@ -14,6 +14,25 @@ document.addEventListener('DOMContentLoaded', function(){
 
     function searchWikipedia(searchTerm){
         const url = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=500&srsearch=${encodeURIComponent(searchTerm)}`;
+
+        fetch(url).then(response => response.json()).then(data => {
+            displayResults(data.query.search);
+        }).catch(error => alert('Error : ' * error));
     }
 
-})
+    function displayResults(results) {
+        resultsContainer.innerHTML = '';
+        resultsCounter.textContent = `Results Count : ${results.length}`;
+        results.forEach(result => {
+            const resultElement = document.createElement('div');
+            resultElement.className = 'result';
+            resultElement.innerHTML = `
+            
+            <h3>${result.title}</h3>
+            <p>${result.snippet}</p>
+            <a href="https://en.wikipedia.org/?curid=${result.pageid}" target="_blank">Read More</a>
+            `;
+            resultsContainer.appendChild(resultElement);
+        });
+    }
+});
